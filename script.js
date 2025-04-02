a93898f2acaf91bb6b5f9631b463e1c4bcae4506
tltnsk <ateletanska@yahoo.com>
2025-04-03 00:51:08 +0300

    Add mouse event

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let container = document.getElementById('container');
+let container = document.getElementById('container');
 
+function createGrid(size) {
     container.innerHTML = '';
-    let size = 16;
 
-    const squareSize = 960 / size;
+    const squareSize = (960 / size) + 'px';
 
     for (let i = 0; i < size * size; i++) {
         const gridSquare = document.createElement('div');
 
         gridSquare.classList.add('grid-item');
-        gridSquare.style.width = `${squareSize}px`;
-        gridSquare.style.height = `${squareSize}px`;
+        gridSquare.style.width = squareSize;
+        gridSquare.style.height = squareSize;
 
         container.appendChild(gridSquare);
+        gridSquare.addEventListener('mouseenter', () => colorSquare(gridSquare));
     }
-}); 
+};
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function colorSquare(gridSquare) {
+    gridSquare.style.backgroundColor = getRandomColor();
+}
+
+createGrid(16);
